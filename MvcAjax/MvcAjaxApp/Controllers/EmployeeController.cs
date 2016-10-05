a74f48f56faf4510ae8625ba97631f27a64a22df
TeamOfStars <bilisimegitim15@gmail.com>
2016-10-05 13:55:20 +0300

    MVC ajax json
    MVC ajax json

---
+++ MvcAjax/MvcAjaxApp/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcAjaxApp.Models;
+
+namespace MvcAjaxApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ReturnList()
+        { 
+            NORTHWNDContext db=new NORTHWNDContext();
+
+            //var emplist = db.Employees.ToList();
+            var emplist2 = (from t in db.Employees select new { t.FirstName, t.LastName }).ToList();
+
+
+            return Json(emplist2, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult ReturnEmploye(int Id)
+        {
+            NORTHWNDContext db = new NORTHWNDContext();
+            var emplist2 = (from t in db.Employees  where t.EmployeeID==Id select new { t.FirstName, t.LastName,t.Title }).FirstOrDefault();
+            
+
+            return Json(emplist2, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MvcAjax/MvcPizzaYap/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcPizzaYap.Models;
+
+namespace MvcPizzaYap.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        static SiparisEntities db;
+        static HomeController()
+        {
+            db = new SiparisEntities();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UyeKayit()
+        {
+            Uye uye = new Uye();
+            return View(uye);
+        }
+        [HttpPost]
+        public JsonResult UyeKayit(Uye uye)
+        {
+
+            db.Uye.Add(uye);
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult UyeGiris()
+        {
+            return View();
+        }
+
+    }
+}
